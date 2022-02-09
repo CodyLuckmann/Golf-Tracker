68ba2442a473a39b0e8b18d987ce50caced86167
CodyLuckmann <cluckmann@live.com>
2022-02-09 13:54:13 -0600

    fix: updated url path to match create_course

--- drf_jwt_capstone_backend/course/urls.py
+++ drf_jwt_capstone_backend/course/urls.py
@@ -3,7 +3,7 @@ from course import views
 
 urlpatterns = [
     path('all/', views.get_all_courses ),
-    path('', views.user_courses ),
+    path('', views.create_course ),
     path('update/<int:course_id>', views.update_course),
     path('delete/<int:course_id>', views.delete_course)
 ]
