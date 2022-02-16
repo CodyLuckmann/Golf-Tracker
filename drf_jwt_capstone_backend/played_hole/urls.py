a4c8d3e48b663ba846419a80ad87d0081e18e363
CodyLuckmann <cluckmann@live.com>
2022-02-16 16:03:44 -0600

    feat: set up url paths to created views

--- drf_jwt_capstone_backend/played_hole/urls.py
+++ drf_jwt_capstone_backend/played_hole/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('user/', views.get_played_hole_by_user),
     path('update/<int:played_hole_id>', views.update_played_hole),
     path('delete/<int:played_hole_id>', views.delete_played_hole),
-    path('date/<str:dt>', views.get_hole_by_date)
+    path('date/<int:course_id>/<str:dt>', views.get_hole_by_date),
+    path('putts/<int:course_id>/<str:dt>', views.get_total_putts),
+    path('alldates/<int:course_id>', views.get_all_dates),
 ]
